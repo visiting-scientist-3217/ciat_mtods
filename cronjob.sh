ab49d7b4690f6df6486ed63d8cba1fde85e7ac99
visiting-scientist-3217 <l.hagemann@cgiar.org>
2017-07-10 07:39:08 -0500

    forgot to export LD_LIB..

---
+++ cronjob.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CASSG_HOME=/home/cassghub
+MTODS_HOME=$CASSG_HOME/software/mtods
+
+execute () { 
+    echo -e "\n--- `date`\n"
+    cd $MTODS_HOME
+    . $CASSG_HOME/.bashrc
+    . .env
+    ./mtods.py -v
+} > $MTODS_HOME/cronjob.log 2>&1
+
+execute
+
+# vim: ts=4 sts=4 sw=4 et
